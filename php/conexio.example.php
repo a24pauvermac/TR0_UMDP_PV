32aa0e94169e532f3154ddd319cccb568886215f
a24pauvermac <a24pauvermac@inspedralbes.cat>
2025-10-05 22:15:14 +0200

    Corregit el gitignore

--- php/conexio.example.php
+++ php/conexio.example.php
@@ -1,15 +1,20 @@
 <?php
+// Aquest és un fitxer d'exemple per a la configuració de la base de dades
+// Copia aquest fitxer com a 'conexio.php' i configura les teves credencials
+
 $hostname = gethostname();
 if (strlen($hostname) === 12 && ctype_alnum($hostname)) {
+    // Configuració per a Docker
     $host = 'mysql';
     $db   = 'quiz_banderas';
     $user = 'quiz_user';
     $pass = 'quiz_password';
 } else {
+    // Configuració per a entorn local
     $host = 'localhost';
-    $db   = 'a24pauvermac_kahoot';
-    $user = 'a24pauvermac_kahoot';
-    $pass = '{_&hbz:sNo)tURQ5';
+    $db   = 'nom_de_la_teva_base_de_dades';
+    $user = 'el_teu_usuari';
+    $pass = 'la_teva_contrassenya';
 }
 
 $conn = new mysqli($host, $user, $pass, $db);
